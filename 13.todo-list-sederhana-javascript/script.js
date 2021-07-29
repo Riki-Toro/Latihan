8cf55d74579afaf5d064bc74382bdf78655f4a77
Riki-Toro <rikitoro22@gmail.com>
2021-07-29 11:34:50 +0700

    refactoring

---
+++ 13.todo-list-sederhana-javascript/script.js
@@ -0,0 +1,39 @@
+const tombol = document.querySelector('.tombol');
+        
+function buatList(input) {
+    const table = document.querySelector('table');
+    const tr = document.createElement('tr');
+    const td = document.createElement('td');
+    
+    const inputToDo = document.createElement('td');
+    inputToDo.append(input);
+
+    table.append(tr);
+    tr.append(inputToDo, tombolHapus());
+}
+
+function tombolHapus() {
+    return buatSelesai(function(event) {
+        clear(event.target.parentElement);
+    });
+}
+function buatSelesai(eventListener) {
+    const btn = document.createElement('input');
+    btn.setAttribute('value', 'hapus');
+    btn.setAttribute('type', 'submit');
+    btn.addEventListener('click', function(event) {
+        eventListener(event);
+    });
+    return btn;
+}
+function clear(element) {
+    element.remove();
+    alert('List berhasil dihapus!');
+}
+
+
+tombol.addEventListener('click', function() {
+    let input = document.getElementById('input').value;
+    
+    buatList(input);
+});
